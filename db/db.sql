6fa38917f21fbe8a7cf0dbe6719f1377f6fe83c6
Sharon Huaman <sharonb.huaman@gmail.com>
2021-01-14 18:59:17 -0500

    add config db

--- db/db.sql
+++ db/db.sql
@@ -6,10 +6,21 @@ create table clients (
 	id integer not null auto_increment,
     name varchar(45) default null,
     last_name varchar(45) default null,
-    age integer default null,
+    invoice_date DATETIME NULL,
     birthdate date not null,
+    age int generated always as (YEAR(invoice_date) - YEAR(birthdate) - IF(STR_TO_DATE(CONCAT(YEAR(invoice_date), '-', MONTH(birthdate), '-', DAY(birthdate)) ,'%Y-%c-%e') > invoice_date, 1, 0)),
     primary key(id)
 );
 
 describe clients;
 
+select * from clients;
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
+
+ALTER TABLE clients DROP COLUMN age;
+
+drop table clients;
+
+DELETE FROM clients where id = 2;
--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'company',
+  host: 'us-cdbr-east-03.cleardb.com',
+  user: 'b4729a60f4a226',
+  password: 'cbf96102',
+  database: 'heroku_212ea951d035dff',
   multipleStatements: true
 });
 
